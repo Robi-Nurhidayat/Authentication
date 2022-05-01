707a02aa933a88f8c655a488541d0e38c42e40b2
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-05-01 10:41:49 +0700

    create register page, page route, and display input form in console log

--- app.js
+++ app.js
@@ -8,11 +8,51 @@ const express = require('express');
 
 const app = express();
 
+app.use(session({
+    secret: "this is a secret",
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/latihan1')
+
+const userSchema = new mongoose.Schema({
+    username: { type: String },
+    password: { type: String }
+});
+
+userSchema.plugin(passportLocalMongoose)
+
+const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
+
 
 app.get('/', function(req, res) {
 
     res.render('home')
+})
+
+
+app.get('/register', function(req, res) {
+    res.render('register')
+})
+
+
+app.post('/register', function(req, res) {
+    console.log(req.body.email)
+    console.log(req.body.password)
 })
 
 app.listen(3000, function() {
